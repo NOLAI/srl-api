fee315a18b292ca2f40167571b8f790acc76d5c3
Stijn Groenen <git@stijngroenen.nl>
2025-01-13 19:23:12 +0100

    Added writing processes and goals endpoint

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.db.config import register_db
 from app.user.routes import router as user_router
 from app.essay.routes import router as essay_router
 from app.process.routes import router as process_router
+from app.goals.routes import router as goals_router
 
 origins = [
     '*'
@@ -17,6 +18,7 @@ def get_application() -> FastAPI:
     _app.include_router(user_router)
     _app.include_router(essay_router)
     _app.include_router(process_router)
+    _app.include_router(goals_router)
     _app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
